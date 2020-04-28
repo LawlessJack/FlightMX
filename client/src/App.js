e4c0cd383476cb9eb26cb4fe784965d2b1ad1080
Jack Lawless <Lawless.jackm@gmail.com>
2020-04-28 18:29:27 -0500

    updated landing page

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import background from './assets/images/sr71.jpg';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
-// import './App.css';
+import Nav from "./components/Nav"
+import Footer from "./components/Footer"
+import './App.css';
 
 
 function App() {
   return (
+    <div className="background">
     <div className="App">
       <header className="App-header">
+        <Nav />
         <Jumbotron>
           <h2>FlightMX</h2>
           <p>by the A-Team! We pity the foo...</p>
@@ -16,7 +20,10 @@ function App() {
         </Jumbotron>
         <img src={background} className="App-logo" alt="logo" />
       </header>
-    </div>
+      <Footer />
+      </div>
+      </div>
+      
   );
 }
 
